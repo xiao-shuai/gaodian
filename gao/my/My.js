a494cea9f115cb98c323fccc7d55d3fa4918406f
syu <1838653805@qq.com>
2019-09-09 14:20:58 +0800

    merge

---
+++ gao/my/My.js
@@ -0,0 +1,32 @@
+import React,{Component} from 'react'
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    Image,
+    ScrollView,
+    StyleSheet,
+    ActivityIndicator,
+    TextInput,AsyncStorage,
+    SafeAreaView,Alert,Linking,Modal,ProgressViewIOS
+} from 'react-native'
+
+class My extends Component{
+
+    constructor(props){
+        super(props)
+        this.state={
+
+        }
+    }
+
+    render(){
+        return(
+            <SafeAreaView style={{flex:1}}>
+
+            </SafeAreaView>
+        )
+    }
+}
+
+export default My
